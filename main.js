f42cf7f17b5be81108a2de2fcf64d557cafab58f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-12-08 14:30:35 -0500

    working on solutions

---
+++ main.js
@@ -0,0 +1,34 @@
+const underLine = document.querySelectorAll('.feature2 ol li')
+console.dir(underLine)
+
+const crossOut = (event) => {
+    const crossedOut = event.target
+    crossedOut.style.textDecoration = 'line-through';
+}
+
+for (const features of underLine) {
+    features.addEventListener('click', crossOut);
+}
+
+
+const ghostMode = document.querySelectorAll('.feature3 ul li')
+
+const casperTheGhost = (event) => {
+    const casperTheFriendlyGhost = event.target
+    casperTheFriendlyGhost.style.opacity = '0.50';
+}
+
+for (const opacity of ghostMode) {
+    opacity.addEventListener('click', casperTheGhost);
+}
+
+
+
+
+
+
+
+
+
+
+
